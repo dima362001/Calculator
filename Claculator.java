7c3c03e984fc6304e505cd2a91ec62140b91e13f
Dima <dima.m.aqel@gmail.com>
2023-12-02 20:36:31 +0000

    Features added by dev1

---
+++ Claculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+
+    public static int add (int a, int b){
+        int c = a + b;
+        return c;
+    }
+
+    public static int sub(int a, int b){
+        int c = a-b;
+        return c;
+    }
+}

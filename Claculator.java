2913caeb195481a6d7267e95f1e32dbae434bcb0
Dima <dima.m.aqel@gmail.com>
2023-12-02 20:42:18 +0000

    Features added by dev3

---
+++ Claculator.java
@@ -0,0 +1,8 @@
+   public static int mul(int a, int b){
+        int c = a *b;
+        return c;
+    }
+    public static double div (int a, int b){
+        double c = a/b;
+        return c;
+    }
